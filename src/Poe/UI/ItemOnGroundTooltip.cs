058e1071b286b7cd77e3394cb27dd51c8ff25683
Sirais <github@huede.org>
2015-01-05 08:48:43 +0100

    ItemOnGround

---
+++ src/Poe/UI/ItemOnGroundTooltip.cs
@@ -0,0 +1,23 @@
+﻿namespace PoeHUD.Poe.UI
+{
+    public class ItemOnGroundTooltip : Element
+    {
+        public Entity Item
+        {
+            get
+            {
+                var address = m.ReadInt(Address + OffsetBuffers, 0, 0x964, 0x974);
+                var entity = GetObject<Entity>(address);
+                return entity;
+            }
+        }
+
+        public Element ToolTip
+        {
+            get
+            {
+                return GetChildAtIndex(0);
+            }
+        }   
+    }
+}
